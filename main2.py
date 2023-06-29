dd2b71668dfd2c65460475168085a909495275ca
IngenieroRicardo <138132387+IngenieroRicardo@users.noreply.github.com>
2023-06-29 08:34:18 -0600

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,6 @@
+print("Ingrese un numero:")
+numero = int(input())
+multiplicacion = 0
+for x in range(1,11):
+  multiplicacion = numero * x
+  print(str(numero)+" x "+str(x)+" = "+str(multiplicacion))
